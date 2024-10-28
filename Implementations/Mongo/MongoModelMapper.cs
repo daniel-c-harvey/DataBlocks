a6b7406db236ea47c7e73b6d73474972500e97fa
Daniel Harvey <work.daniel.harvey@gmail.com>
2024-10-28 18:42:01 -0400

    WIP testing model mapping, non-functioning

--- Implementations/Mongo/MongoModelMapper.cs
+++ Implementations/Mongo/MongoModelMapper.cs
@@ -13,31 +13,45 @@ namespace DataAccess
     {
         internal static void RegisterModel<TModel>()
         {
-            RegisterDerivedTypes<TModel>(typeof(TModel));
+            RegisterDerivedTypes(typeof(TModel));
+
+            foreach(Type type in typeof(TModel).GetMembers(BindingFlags.Public | BindingFlags.Instance).Where(m => m.MemberType == (MemberTypes.Property | MemberTypes.Field)).Select(m => m.GetType()))
+            {
+                RegisterDerivedTypes(type);
+
+                if (type.IsGenericType)
+                {
+                    foreach(Type genericType in type.GetGenericArguments())
+                    {
+                        RegisterDerivedTypes(genericType);
+                    }
+                }
+            }
         }
 
-        private static void RegisterDerivedTypes<TModel>(Type baseType)
-        {;
+        private static void RegisterDerivedTypes(Type baseType)
+        {
             var derivedTypes = Assembly.GetAssembly(baseType)?
                                        .GetTypes()
                                        .Where(t => !t.IsAbstract && baseType.IsAssignableFrom(t))
                                        .ToList() ?? [];
+            
+            BsonClassMap cm = new(baseType);
 
-            BsonClassMap.RegisterClassMap<TModel>(cm =>
+            cm.AutoMap();
+
+            // Register each derived type as a known type
+            foreach (var derivedType in derivedTypes)
             {
-                cm.AutoMap();
+                cm.AddKnownType(derivedType);
+            }
 
-                // Register each derived type as a known type
-                foreach (var derivedType in derivedTypes)
-                {
-                    cm.AddKnownType(derivedType);
-                }
-            });
+            BsonClassMap.RegisterClassMap(cm);
         }
 
-        private static bool IsKnown(Type source, Type target)
-        {
-            return target.IsSubclassOf(source) || source.GetFields().Aggregate(true, (sofar, next) => sofar || IsKnown(next.FieldType, target));
-        }
+        //private static bool IsKnown(Type source, Type target)
+        //{
+        //    return target.IsSubclassOf(source) || source.GetFields().Aggregate(true, (sofar, next) => sofar || IsKnown(next.FieldType, target));
+        //}
     }
 }
